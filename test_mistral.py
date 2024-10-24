16e394b4cf76aa02ac635374790be17efe1b1e3a
Mukundan <amukundan@r-lnx214.cs.uiowa.edu>
2024-10-24 12:33:58 -0500

    updated mistral

--- test_mistral.py
+++ test_mistral.py
@@ -10,11 +10,7 @@ import test_database as tdb
 import mongo_database as mongo_db
 
 
-def RBIC_mistral(title, post):
-    mistral_models_path = '/local/disk2/not_backed_up/adi/mistral_models/7B-Instruct-v0.3/'
-    tokenizer = MistralTokenizer.from_file(f"{mistral_models_path}/tokenizer.model.v3")
-    model = Transformer.from_folder(mistral_models_path)
-
+def RBIC_mistral(model, tokenizer, title, post):
     #first prompt
     messages = [
         SystemMessage(content="Your role is to understand the cause and effect relationship in social media posts."),
@@ -73,16 +69,22 @@ def test_RBIC_mistral(db, collection):
     posts = tdb.get_posts_in_subreddits(collection, ["opiatesrecovery", "opiates"])
     new_collection = db['gist_test']
     i = 0
+    mistral_models_path = '/local/disk2/not_backed_up/adi/mistral_models/7B-Instruct-v0.3/'
+    tokenizer = MistralTokenizer.from_file(f"{mistral_models_path}/tokenizer.model.v3")
+    model = Transformer.from_folder(mistral_models_path)
     for post in posts:
         i+=1
         if i%10 == 0:
             print(f"Processed {i} posts")
+        if i%100 == 0:
+            with open("error_log.txt", "a") as log_file:
+                log_file.write(f"Processed {i} posts\n")
         title = post["title"]
         selftext = post["selftext"]
         selftext = mongo_db.preprocess_post(selftext)
         if not selftext or selftext == "[removed]":
             continue
-        mistral_output = RBIC_mistral(title, selftext)
+        mistral_output = RBIC_mistral(model, tokenizer, title, selftext)
         new_obj = post.copy()
         if mistral_output:
             new_obj["has_gist"] = True
